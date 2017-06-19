7fc27e927bc5e13d9fc7b2144f1b084aa71c4157
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-06-19 19:48:15 +0200

    add clusterinfo command

--- justin/cli/cli.go
+++ justin/cli/cli.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-  "fmt"
   "os"
+  "fmt"
+  "sort"
+  "io/ioutil"
+  "net/http"
 
   "github.com/urfave/cli"
 )
@@ -11,10 +14,26 @@ func main() {
   app := cli.NewApp()
   app.Name = "JustinDB-Cli"
   app.Usage = "Manage JustinDB from Command Line"
-  app.Action = func(c *cli.Context) error {
-    fmt.Println("Cli!")
-    return nil
+  url := "http://localhost:9000/cluster"
+
+  app.Commands = []cli.Command{
+    {
+      Name:    "clusterinfo",
+      Aliases: []string{"ci"},
+      Usage:   "Status of the cluster",
+      Action:  func(c *cli.Context) error {
+        resp, _ := http.Get(url)
+        defer resp.Body.Close()
+        bodyBytes, _ := ioutil.ReadAll(resp.Body)
+        bodyString   := string(bodyBytes)
+        fmt.Println(bodyString)
+        return nil
+      },
+    },
   }
+
+  sort.Sort(cli.FlagsByName(app.Flags))
+  sort.Sort(cli.CommandsByName(app.Commands))
 
   app.Run(os.Args)
 }
