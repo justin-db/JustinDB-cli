75e302be94b605d4408471b59bcfb5e12ec053fa
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-06-19 19:50:36 +0200

    add health command

--- justin/cli/cli.go
+++ justin/cli/cli.go
@@ -14,15 +14,28 @@ func main() {
   app := cli.NewApp()
   app.Name = "JustinDB-Cli"
   app.Usage = "Manage JustinDB from Command Line"
-  url := "http://localhost:9000/cluster"
+  url := "http://localhost:9000"
 
   app.Commands = []cli.Command{
     {
       Name:    "clusterinfo",
       Aliases: []string{"ci"},
-      Usage:   "Status of the cluster",
+      Usage:   "Shows status of the cluster",
       Action:  func(c *cli.Context) error {
-        resp, _ := http.Get(url)
+        resp, _ := http.Get(url + "/cluster")
+        defer resp.Body.Close()
+        bodyBytes, _ := ioutil.ReadAll(resp.Body)
+        bodyString   := string(bodyBytes)
+        fmt.Println(bodyString)
+        return nil
+      },
+    },
+    {
+      Name:    "nodehealth",
+      Aliases: []string{"nh"},
+      Usage:   "Display health of the node",
+      Action:  func(c *cli.Context) error {
+        resp, _ := http.Get(url + "/health")
         defer resp.Body.Close()
         bodyBytes, _ := ioutil.ReadAll(resp.Body)
         bodyString   := string(bodyBytes)
